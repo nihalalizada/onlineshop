eed06175fa02bc680254be93c7b7f10a6faa6f4e
ahmedagdmoun <ahmed.agdmoun@outlook.com>
2022-05-29 10:14:58 +0200

    added springdoc dependency and swagger-ui configuration

--- app/src/main/java/com/example/onlineshop/OnlineShopApplication.java
+++ app/src/main/java/com/example/onlineshop/OnlineShopApplication.java
@@ -1,9 +1,11 @@
 package com.example.onlineshop;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition
 public class OnlineShopApplication {
 
     public static void main(String[] args) {

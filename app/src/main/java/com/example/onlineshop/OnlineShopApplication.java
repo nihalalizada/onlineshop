a682bfa56f8c823fec009eb6371eede2428d62fd
ahmedagdmoun <ahmed.agdmoun@outlook.com>
2022-06-02 21:08:04 +0200

    added Jasypt to encrypt sensitive data in properties file

--- app/src/main/java/com/example/onlineshop/OnlineShopApplication.java
+++ app/src/main/java/com/example/onlineshop/OnlineShopApplication.java
@@ -1,11 +1,13 @@
 package com.example.onlineshop;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @OpenAPIDefinition
+@EnableEncryptableProperties
 public class OnlineShopApplication {
 
     public static void main(String[] args) {
